7ab414a048a5344bfd362c4c4cc2d4eb88f3d10f
MavapeGZ <mavape4403@gmail.com>
2024-05-31 14:31:57 +0300

    Upload first files

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow, Menu} = require('electron')
+console.warn("main process")
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile("index.html");
+    win.webContents.openDevTools();
+
+}
+
+app.whenReady().then(createWindow)
